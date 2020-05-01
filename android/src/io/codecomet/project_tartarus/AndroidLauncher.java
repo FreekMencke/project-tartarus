d7accf49d173cc821351ba1f34acf0eee5a448e2
Freek Mencke <freek_mencke@outlook.com>
2020-05-01 20:36:53 +0200

    initial commit

---
+++ android/src/io/codecomet/project_tartarus/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.codecomet.project_tartarus;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.codecomet.project_tartarus.ProjectTartarus;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectTartarus(), config);
+	}
+}
---
+++ desktop/src/io/codecomet/project_tartarus/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.codecomet.project_tartarus.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import io.codecomet.project_tartarus.ProjectTartarus;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new ProjectTartarus(), config);
+	}
+}
